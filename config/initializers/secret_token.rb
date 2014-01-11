a523b256958f22309acd598150becd83440f646b
Nicholas Valbusa <squallstar@gmail.com>
2014-01-11 00:12:38 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paperboard::Application.config.secret_key_base = '4257b5369ef4976a246542422b673184bef74215116c9a16a194f79cc432fec73f2a1887d7133643c24b4b757415bc48ba6cace102bb6cfd9272361403503953'
