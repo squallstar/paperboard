6d11a81d179304a77c73280b6d45b07033349c28
Nicholas Valbusa <info@squallstar.it>
2014-01-13 20:01:28 +0000

    corrected validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ActiveRecord::Base
   has_many :users, through: :project_members
   has_many :members, foreign_key: :project_id, class_name: :ProjectMember, dependent: :destroy
   
-  validates :name, null: false
+  validates :name, presence: true
 end
