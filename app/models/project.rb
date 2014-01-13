b840b073230c5a5f95af138910c710bd208ee583
Nicholas Valbusa <info@squallstar.it>
2014-01-13 19:55:26 +0000

    validating project name

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :users, through: :project_members
   has_many :members, foreign_key: :project_id, class_name: :ProjectMember, dependent: :destroy
+  
+  validates :name, null: false
 end
