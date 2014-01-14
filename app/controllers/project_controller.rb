e242c8ab3f4b2f6cb047404b4e8aad2c65905fea
Nicholas Valbusa <info@squallstar.it>
2014-01-14 17:59:28 +0000

    skeleton reset (tmp)

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -9,7 +9,7 @@ class ProjectController < ApplicationController
   def set_project
     @project = @current_user.project_with_id(params[:id])
     if not @project
-      redirect_to projects_path, notice: 'That project does not exist or you don\'t have the rights to view it'
+      redirect_to projects_path, alert: 'That project does not exist or you don\'t have the rights to view it'
     end
   end
 end
