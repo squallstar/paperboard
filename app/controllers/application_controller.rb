e1ebf20098a2e11021b5ca1a169f2e3fea91f8c4
Nicholas Valbusa <squallstar@gmail.com>
2014-01-19 21:25:00 +0000

    view rendering time in footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :set_start_time
   before_action :authorize
 
   attr_reader :current_user
@@ -16,8 +17,11 @@ class ApplicationController < ActionController::Base
   end
 
   private
+    def set_start_time
+      @start_time = Time.now.usec
+    end
 
-  def current_user
-    @current_user ||= User.select(:id, :first_name, :last_name).find_by(id: session[:user_id])
-  end
+    def current_user
+      @current_user ||= User.select(:id, :first_name, :last_name).find_by(id: session[:user_id])
+    end
 end
