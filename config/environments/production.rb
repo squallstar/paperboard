bf5243fe6742374cd4a51ad524ba36537c890746
Nicholas Valbusa <squallstar@gmail.com>
2014-01-22 01:09:47 +0000

    trying a new url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,8 @@ Paperboard::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :url =>'paperboard-development.s3.amazonaws.com'
+    :url =>'//paperboard-development.s3.amazonaws.com',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 
   config.action_mailer.delivery_method = :smtp
