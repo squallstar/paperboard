a689181805568c7f3e3f8f40d2f29c73c34abf41
Nicholas Valbusa <squallstar@gmail.com>
2014-01-22 00:45:47 +0000

    missing paperclip settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,15 @@ Paperboard::Application.configure do
     :host => "heroku.paperboard.me"
   }
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.mandrillapp.com',
