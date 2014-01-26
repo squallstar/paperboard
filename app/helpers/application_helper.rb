940a0fed058b6a6f512e6c0f9a1d99a136af70cf
Nicholas Valbusa <squallstar@gmail.com>
2014-01-26 18:30:47 +0000

    plans helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-
-  def plan_features(plan)
-    set = AppSettings.plans[plan]
-    I18n.translate("plans.features",
-      projects: set.n_projects > 0 ? set.n_projects : 'Unlimited',
-      team: set.n_teams > 0 ? set.n_teams : 'unlimited',
-      users: set.n_users > 0 ? set.n_teams : 'unlimited'
-    )
-  end
-
 end
