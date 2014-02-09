33991dac733e9ecc18f4aa70070d4179a35e8e6e
Nicholas Valbusa <squallstar@gmail.com>
2014-02-09 13:06:23 +0000

    billing css

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -4,7 +4,8 @@ module PlansHelper
     I18n.translate("plans.features",
       projects: set.n_projects > 0 ? set.n_projects : 'Unlimited',
       team: set.n_teams > 0 ? set.n_teams : 'unlimited',
-      users: set.n_users > 0 ? set.n_teams : 'unlimited'
+      users: set.n_users > 0 ? set.n_users : 'unlimited',
+      count: set.n_projects
     )
   end
 end
