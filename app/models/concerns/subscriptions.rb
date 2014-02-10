8833e2fc36c3c6abc925c606c4683dfcaaacb5bf
Nicholas Valbusa <info@squallstar.it>
2014-02-10 16:33:42 +0000

    plans for subscriptions

--- app/models/concerns/subscriptions.rb
+++ app/models/concerns/subscriptions.rb
@@ -5,7 +5,11 @@ module Subscriptions
     has_one :subscription, dependent: :destroy
   end
 
-  def plan
+  # Calling .plan gives the plan name, while passing "projects"
+  # "teams" or "users" will return the permitted number.
+  def plan(key = nil)
     @plan ||= subscription ? subscription.plan.name : "free"
+    return @plan unless key
+    AppSettings.plans[@plan]["n_#{key}"]
   end
 end
